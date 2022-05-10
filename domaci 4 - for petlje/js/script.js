9f1f6e073e665f64c3fa5a0f657002940a1b0482
Aleksa Fisic <fiske92@live.com>
2022-05-10 20:31:18 +0200

    domaci for petlje

---
+++ domaci 4 - for petlje/js/script.js
@@ -0,0 +1,53 @@
+let niz = [1, 2 ,3];
+
+let prazanNiz;
+
+function mapirajNiz(nekiNiz) {
+    if (Array.isArray(nekiNiz) == false) {
+        nekiNiz = [];
+        console.log(nekiNiz);
+        return nekiNiz;  // iz nekog razloga ne vraca niz, pa sam morao preko console.log
+    }
+
+    let noviNiz = [];
+    for(let i = 0; i < nekiNiz.length; i++){
+        nekiNiz[i] = nekiNiz[i] + "IZMENJENO";
+        noviNiz.push(nekiNiz[i]);
+    }
+
+    console.log(noviNiz);
+
+}
+
+mapirajNiz(niz);
+
+mapirajNiz(prazanNiz);
+
+
+///////////////////////////////////////////////
+
+function zbirOpsega(odBroja, doBroja) {
+    let ukupniZbir = odBroja;
+    for(let i = (odBroja + 1); i < doBroja; i++) {
+        ukupniZbir = ukupniZbir + i;
+    }
+    console.log(ukupniZbir);
+    return ukupniZbir; // return ne vraca, negde gresim ?
+}
+zbirOpsega(4,7);
+
+/////////////////////////////////////////////
+
+let niz1 = [1,2,3];
+let niz2 = [4,5,6];
+
+function myFunction(prviNiz, drugiNiz) {
+    let noviNiz = [];
+     for (let i = 0; i < 3; i++) {
+         let clanNovogNiza = prviNiz[i] + drugiNiz[i];
+         noviNiz.push(clanNovogNiza);
+     }
+     console.log(noviNiz)
+}
+
+myFunction(niz1, niz2);
